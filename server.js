8da442a09757bdcb19416081a0f3f038e3bcea2f
Merekeyev Dias <dikxarper.programming@gmail.com>
2024-07-25 21:37:42 +0500

    added view engine

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 require('dotenv').config({path: './config/.env'});
 
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.use('/views', express.static(path.join(__dirname + '/views')));
+app.use('/public', express.static(path.join(__dirname + '/public')));
+
 app.get('/', (req, res) => {
-    res.send("This is server!")
+    res.render('index');
 })
 
 app.listen(process.env.PORT, function() {
