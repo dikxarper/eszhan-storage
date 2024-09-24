5fe47ea0906258c920c5212a4b3969aece5e3de3
eszhan <jasarcamat@gmail.com>
2024-09-24 15:44:20 +0600

    added confirm page

--- server.js
+++ server.js
@@ -1,32 +1,35 @@
-const express = require('express');
-const expressLayouts = require('express-ejs-layouts');
-const path = require('path');
-const bodyParser = require('body-parser');
-require('dotenv').config({path: './config/.env'});
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const path = require("path");
+const bodyParser = require("body-parser");
+require("dotenv").config({ path: "./config/.env" });
 const app = express();
 
-app.engine('html', require('ejs').renderFile);
+app.engine("html", require("ejs").renderFile);
 
-app.set('view engine', 'html');
-app.set('layout', 'layouts/layout')
+app.set("view engine", "html");
+app.set("layout", "layouts/layout");
 
-app.use('/views', express.static(path.join(__dirname + '/views')));
-app.use('/public', express.static(path.join(__dirname + '/public')));
+app.use("/views", express.static(path.join(__dirname + "/views")));
+app.use("/public", express.static(path.join(__dirname + "/public")));
 
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
-app.use(expressLayouts)
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(expressLayouts);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.render('index');
-})
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-app.get('/cart', (req, res) => {
-    res.render('cart');
-})
+app.get("/cart", (req, res) => {
+  res.render("cart");
+});
+app.get("/confirm", (req, res) => {
+  res.render("confirm");
+});
 
-app.listen(process.env.PORT, function() {
-    console.log(`The server is on http://localhost:${process.env.PORT}`);
-})
+app.listen(process.env.PORT, function () {
+  console.log(`The server is on http://localhost:${process.env.PORT}`);
+});
